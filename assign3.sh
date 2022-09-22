b2651bd44fad7beed7a6a41da4bd1718453e3fad
yongsoocho <yongsoocho578@gmail.com>
2022-09-22 14:30:44 +0900

    comple 3

--- assign3.sh
+++ assign3.sh
@@ -1,31 +1,25 @@
-#! /bin/bash
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "Invalid Input"
+else
+	if [ ${1} -lt 0 ]
+	then
+		echo "Input must be greater than 0"
+	else
+		R=1
+		while [ ${R} -le ${1} ]; do
+			C=1
+			while [ ${C} -le ${2} ]; do
+				result=$(( $R * $C ))
+				echo "${R} * ${C} = ${result}"
+				C=$(( C + 1 ))
+			done
+			R=$(( R + 1 ))
+		done
+	fi
 
 
-for((r=1; r<=$((1)); i++))
-do
-	for((c=1; c<=$((2)); c++))
-  	do
-	       	result=`expr $c \* $r` 
-    		echo "${r} * ${c} = ${result}" 
-  	done
-done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fi
 
--- test.sh
+++ test.sh
@@ -6,3 +6,23 @@ echo $1 $2
 echo "${1} and~ ${2}"
 # echo "${ip1} and ${ip2}"
 
+
+START=1
+END=5
+
+# Now loop it
+for i in {$START..$END}
+do
+   echo "$i"
+done
+
+j=80
+while [ "$j" -le 101 ]; do
+        echo "$j"
+	j=$(( j + 1 ))
+done
+
+for (( c=$START; c<=$END; c++ ))
+do
+	echo "$c"
+done
